7297497455ece34069dd47479a71b364885cf970
Sweetcheeks12354 <goodrichnik@gmail.com>
2023-03-02 20:21:41 -0500

    Updated go files

--- Niks_dev/init.sql
+++ Niks_dev/init.sql
@@ -1,29 +1,12 @@
-
-CREATE TABLE IF NOT EXISTS taxi_trips (
-  company VARCHAR(255),
-  dropoff_census_tract VARCHAR(255),
-  dropoff_centroid_latitude NUMERIC(10, 7),
-  dropoff_centroid_location POINT,
-  dropoff_centroid_longitude NUMERIC(10, 7),
-  dropoff_community_area INTEGER,
-  extras NUMERIC(10, 2),
-  fare NUMERIC(10, 2),
-  payment_type VARCHAR(255),
-  pickup_census_tract VARCHAR(255),
-  pickup_centroid_latitude NUMERIC(10, 7),
-  pickup_centroid_location POINT,
-  pickup_centroid_longitude NUMERIC(10, 7),
-  pickup_community_area INTEGER,
-  taxi_id VARCHAR(255),
-  tips NUMERIC(10, 2),
-  tolls NUMERIC(10, 2),
-  trip_end_timestamp TIMESTAMP,
-  trip_id VARCHAR(255),
-  trip_miles NUMERIC(10, 2),
-  trip_seconds INTEGER,
-  trip_start_timestamp TIMESTAMP,
-  trip_total NUMERIC(10, 2)
-);
+-- taxi_trips	:	list of taxi trips, with start and end points
+CREATE TABLE IF NOT EXISTS taxi_trips(
+	trip_id				varchar(50) PRIMARY KEY, -- trip id hash 
+	taxi_id				varchar(128), -- taxi id hash
+	trip_seconds			int,	-- trip length in seconds
+	trip_miles			float,	-- not very useful, since this is really based off census tracts...
+	community_area_pickup		int,	-- community area number. blank outside chicago
+	dropoff_community_area		int		-- community area number. blank outside chicago
+)
 
 
 
---
+++ Niks_dev/taxi_trips.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/SebastiaanKlippert/go-soda"
+	_ "github.com/lib/pq"
+)
+
+//https://data.cityofchicago.org/resource/wrvz-psew.json?$where=trip_miles > 0.1
+
+//import API TOKEN
+
+//create a struct to hold the data
+
+type Trip []struct {
+	TripID                  string `json:"trip_id"`
+	TaxiID                  string `json:"taxi_id"`
+	TripSeconds             string `json:"trip_seconds"`
+	TripMiles               string `json:"trip_miles"`
+	PickupCommunityArea     string `json:"pickup_community_area,omitempty"`
+	DropoffCommunityArea     string `json:"dropoff_community_area,omitempty"`
+}
+
+
+
+
+
+func CSVSample(data_set string, query string) Trip {
+	APP_TOKEN := os.Getenv("cqh5tqp0euzwjliw0si085ncm")
+	url_format := fmt.Sprintf("https://data.cityofchicago.org/resource/%s", data_set)
+	sodareq := soda.NewGetRequest(url_format, APP_TOKEN)
+	sodareq.Format = "json"
+	sodareq.Query.Where = query
+	sodareq.Query.Limit = 1000
+
+
+
+	// Gets the response
+	//wait 1 second
+	time.Sleep(1 * time.Second)
+	resp, err := sodareq.Get()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	// Decodes the response
+	var trips Trip
+	err = json.NewDecoder(resp.Body).Decode(&trips)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+
+
+
+
+	return trips
+
+
+}
+
+// main
+
+func main() {
+	// Get today's date
+	//02/01/2023 12:00:00 AM
+	//https://data.cityofchicago.org/api/id/wrvz-psew.json?$query=select *, :id order by `trip_start_timestamp` asc limit 100
+	//Get everything after 2023-01-01
+	
+	where_statement := "trip_start_timestamp > '2022-01-01T00:00:00.000'"
+
+
+	// Get a sample of taxi trips from the Socrata API from today
+	trips := CSVSample("wrvz-psew", where_statement)
+
+	host := "localhost"
+    port := 5432
+    user := "postgres"
+    password := "password"
+    dbname := "mydatabase"
+    connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	// Open a new database connection
+    db, err := sql.Open("postgres", connStr)
+    if err != nil {
+        panic(err)
+    }
+    defer db.Close()
+
+	// Prepare a SQL statement to insert a new row into the 'taxi_trips' table
+	stmt, err := db.Prepare(`INSERT INTO taxi_trips (    
+    trip_id,
+    taxi_id,
+    trip_seconds,
+    trip_miles,
+    community_area_pickup,
+	dropoff_community_area
+	) VALUES ($1, $2, $3, $4, $5, $6)
+	ON CONFLICT (trip_id) DO NOTHING`)
+	if err != nil {
+    log.Fatal(err)
+		}
+	defer stmt.Close()
+
+
+	// Insert each taxi trip into the 'taxi_trips' table using a prepared statement
+	for _, trip := range trips {
+		fmt.Println(trip.TripID)
+		if trip.TripID != "" && trip.TripSeconds != "" && trip.TripMiles != "" && trip.PickupCommunityArea != "" && trip.DropoffCommunityArea != "" && trip.TaxiID != "" {
+			_, err = stmt.Exec(			
+				trip.TripID,
+				trip.TaxiID,
+				trip.TripSeconds,
+				trip.TripMiles,
+				trip.PickupCommunityArea,
+				trip.DropoffCommunityArea,
+				
+			)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("Inserted trip with ID %s\n", trip.TripID)
+	}
+}
+
+}
